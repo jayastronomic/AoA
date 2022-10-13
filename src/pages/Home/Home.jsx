91743e5698cfc39d94f97b06c0a65b75f8ed641e
jayastronomic <61813086+jayastronomic@users.noreply.github.com>
2022-10-12 20:22:27 -0500

    Create play music feature

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Player from "../../components/Player";
+
+const Home = () => {
+  return (
+    <div className="">
+      <Player />
+    </div>
+  );
+};
+
+export default Home;
